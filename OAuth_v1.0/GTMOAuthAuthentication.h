07bb24201223174f6a220b4cc15e1363a09c3811
Marc Haisenko <haisenko@equinux.com>
2015-04-29 18:04:57 +0200

    Restored original license (Apache; https://code.google.com/p/gtm-oauth/source/browse/#svn%2Ftrunk%2FSource). Set deployment target for OS X.

--- OAuth_v1.0/GTMOAuthAuthentication.h
+++ OAuth_v1.0/GTMOAuthAuthentication.h
@@ -1,3 +1,18 @@
+/* Copyright (c) 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // This class implements the OAuth 1.0a protocol for creating and signing
 // requests. http://oauth.net/core/1.0a/
 //
@@ -124,8 +139,8 @@ _EXTERN NSString* const kGTMOAuthSignatureMethodRSA_SHA1  _INITIALIZE_AS(@"RSA-S
 // The verified string can be checked with -boolValue. If the result is false,
 // then the email address is listed with the account on the server, but the
 // address has not been confirmed as belonging to the owner of the account.
-@property (retain) NSString *userEmail;
-@property (retain) NSString *userEmailIsVerified;
+@property (nonatomic, copy) NSString *userEmail;
+@property (nonatomic, copy) NSString *userEmailIsVerified;
 
 // property for using a previously-authorized access token
 @property (nonatomic, copy) NSString *accessToken;
@@ -136,7 +151,7 @@ _EXTERN NSString* const kGTMOAuthSignatureMethodRSA_SHA1  _INITIALIZE_AS(@"RSA-S
 
 // property indicating if this auth has an access token so is suitable for
 // authorizing a request. This does not guarantee that the token is valid.
-@property (readonly) BOOL canAuthorize;
+@property (nonatomic, readonly) BOOL canAuthorize;
 
 // userData is retained for the convenience of the caller
 @property (nonatomic, retain) id userData;
